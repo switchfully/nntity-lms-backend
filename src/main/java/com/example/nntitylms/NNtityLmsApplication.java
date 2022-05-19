a4857e5e8ded5aa07681fc5671d35f00a7569e33
Herbert Coenen <coenenherbert@gmail.com>
2022-05-19 16:33:35 +0200

    first commit

---
+++ src/main/java/com/example/nntitylms/NNtityLmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.nntitylms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NNtityLmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NNtityLmsApplication.class, args);
+    }
+
+}
