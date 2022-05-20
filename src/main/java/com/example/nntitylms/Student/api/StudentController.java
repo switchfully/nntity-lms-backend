2d14abed62c7b388d53a0df46bc762be48b6be6c
Jeremy Alen <alen_jeremy@hotmail.com>
2022-05-20 14:24:08 +0200

    ACC2 Comment dependency related to database Add CrossOrigin for the frontend

--- src/main/java/com/example/nntitylms/Student/api/StudentController.java
+++ src/main/java/com/example/nntitylms/Student/api/StudentController.java
@@ -1,15 +1,13 @@
 package com.example.nntitylms.Student.api;
 
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.MediaType.*;
 
 @RestController
 @RequestMapping(path = "/students")
+@CrossOrigin(origins="http://localhost:3000")
 public class StudentController {
 
     @GetMapping(params = {"email", "password"}, produces = APPLICATION_JSON_VALUE)
